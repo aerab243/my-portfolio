d5805c87c556455a835d2b44ce0e6c6f33863912
aerab243 <aerabenandrasana@gmail.com>
2025-09-30 21:38:30 +0300

    fix: correction vercel.json et syntaxe CommonJS pour API

--- api/contact.js
+++ api/contact.js
@@ -1,8 +1,8 @@
-import { Resend } from 'resend';
+const { Resend } = require('resend');
 
 const resend = new Resend(process.env.RESEND_API_KEY);
 
-export default async function handler(req, res) {
+module.exports = async function handler(req, res) {
   // Log pour débogage
   console.log('Requête reçue:', req.method);
   console.log('Variables env:', {
